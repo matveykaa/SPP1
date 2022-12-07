7a519a25e32bb64cb3afb6dfebe8e5e0f244cb56
matveykaa <101522923+matveykaa@users.noreply.github.com>
2022-12-07 16:09:04 +0300

    add serializers.cs

---
+++ Tracer/Serializers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace TracerLib
+{
+    class Serializers
+    {
+        public class Record
+        {
+            [XmlIgnore]
+            [JsonIgnore]
+            public int ID;
+
+            [XmlElement(ElementName = "Thread")]
+            public ThreadRes res;
+
+            public Record(int key, ThreadRes res)
+            {
+                ID = key;
+                this.res = res;
+            }
+            public Record()
+            {
+            }
+        }
+    }
+}
--- Tracer/TraceResult.cs
+++ Tracer/TraceResult.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Text.Json.Serialization;
 
 
 namespace TracerLib
